7579aeb514e309ff807e8033c9f0b39a14361e7f
Marta Prystatska <tradlisboa@gmail.com>
2024-10-05 23:45:28 +0100

    // Connect MongoDB at default port 27017

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const_express = require('express');
+const express = require('express');
 
 const app = express();
 
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require('http');
-const app = require('./app.js');
-const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const app = require('./app');
 
-const setver =http.createServer(app);
+// Connect MongoDB at default port 27017.
+mongoose
+    .connect('mongodb://localhost:27017/fm2024mongoose')
+    .catch((err) => {
+        console.log('Error in DB connection: ' + err);
+        process.exit(1);
+    });
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log('server is running on port' + port);
+    console.log('server is running on port ' + port);
 });
