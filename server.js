aa24ceec3808c3fbe4f5a760c2aa685c0e7968ec
Marta Prystatska <tradlisboa@gmail.com>
2025-02-07 07:52:22 +0000

    update compose.yaml+ update compose-dev.yaml  +start compose ($sudo docker compose up); const { hostMongo } = 'db_mongo';

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
-
+const { hostMongo } = 'db_mongo';  // ´localhost´
 
 
 // Connect MongoDB at default port 27017.
 mongoose
     // if exist administrator user, use 
    // .connect('username:password@mongodb://localhost:27017/fm2024mongoose'})
-    .connect('mongodb://172.17.0.2:27017/fm2024mongoose')
+    .connect(`mongodb://${hostMongo}:27017/fm2024mongoose`)
     .catch((err) => {
         console.log('Error in DB connection: ' + err);
         process.exit(1);
