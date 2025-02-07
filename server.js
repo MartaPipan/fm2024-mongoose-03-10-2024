e2a7871ad02c737bddfe7087bf5235858930a080
Marta Prystatska <tradlisboa@gmail.com>
2025-02-07 13:43:15 +0000

    Add security file (dotenv) .env + example.env; create start-dev.sh+start.sh

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+require('dotenv').config();
 const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
-const { hostMongo } = 'db_mongo';  // ´localhost´
 
 
 // Connect MongoDB at default port 27017.
 mongoose
     // if exist administrator user, use 
    // .connect('username:password@mongodb://localhost:27017/fm2024mongoose'})
-    .connect(`mongodb://${hostMongo}:27017/fm2024mongoose`)
+    .connect(`mongodb://${process.env.DB_HOST_MONGO}:27017/${process.env.DB_MONGO}`)
     .catch((err) => {
         console.log('Error in DB connection: ' + err);
         process.exit(1);
